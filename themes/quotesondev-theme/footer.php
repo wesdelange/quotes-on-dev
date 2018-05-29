5029d2b6d50544701b0d54a34a43eceba91ba9a9
Wes <wes.delange@gmail.com>
2018-05-29 16:29:28 -0400

    Added fonts, set variables and styled footer

--- themes/quotesondev-theme/footer.php
+++ themes/quotesondev-theme/footer.php
@@ -10,8 +10,14 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<?php esc_html( 'Primary Menu' ); ?>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+				</nav><!-- #site-navigation -->
+				
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+					<p>Brought to you by<a href="<?php echo esc_url( 'https://redacademy.com' ); ?>"> RED Academy</a></p>
+					
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
