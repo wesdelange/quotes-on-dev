1eca12c5255b718bd712e4962bdad25e25ffcbe4
Wes <wes.delange@gmail.com>
2018-05-30 11:43:01 -0400

    added source name and source link to content.php

--- themes/quotesondev-theme/template-parts/content.php
+++ themes/quotesondev-theme/template-parts/content.php
@@ -5,14 +5,44 @@
  * @package QOD_Starter_Theme
  */
 
+$source = get_post_meta( get_the_ID(), '_qod_quote_source', true );
+
+$source_url = get_post_meta( get_the_ID(), '_qod_quote_source_url', true);
+
+/*
+echo esc_html ( $source );
+echo esc_html( $source_url );
+*/
+
+
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-	</header><!-- .entry-header -->
 
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
 	<div class="entry-content">
-		<i class="fa fa-envelope"><?php the_excerpt(); ?></i>
+		<?php the_excerpt(); ?>
 	</div><!-- .entry-content -->
+
+	<div class="entry-meta">
+		
+		<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+
+		<?php if( $source && $source_url ) : ?>
+			<span class="source">, <a href="<?php echo $source_url; ?> "><?php echo $source; ?></a></span>
+		<?php elseif ( $source ) : ?>
+			<span class="source">, <?php echo $source; ?></span>
+		<?php else : ?>
+			<span></span>
+		<?php endif; ?>
+
+	</div>	<!-- entry-meta -->
+
+
 </article><!-- #post-## -->
+
+<!-- Button to show another quote -->
+<?php if ( is_home() || is_single() ) : ?>
+	<button type="button" id="new-quote-button">Show Me Another!</button>
+<?php endif; ?>
