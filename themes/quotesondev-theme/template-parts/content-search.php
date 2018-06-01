ba3351345ebaed22dedd3a6106e733d237699cb1
Wes <wes.delange@gmail.com>
2018-06-01 14:42:35 -0400

    styled search results page

--- themes/quotesondev-theme/template-parts/content-search.php
+++ themes/quotesondev-theme/template-parts/content-search.php
@@ -11,11 +11,7 @@
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php qod_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php qod_posted_by(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
+
 	</header><!-- .entry-header -->
 
 	<div class="entry-summary">
